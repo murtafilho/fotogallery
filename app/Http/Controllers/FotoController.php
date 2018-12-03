c1c87d1716ef2c8982a0e425a461f95e4f785205
Roberto Murta <murtafilho@gmail.com>
2018-12-03 21:12:37 -0200

    Foto renamed

---
+++ app/Http/Controllers/FotoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Foto;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+
+class FotoController extends Controller
+{
+    public function index()
+    {
+        $fotos = DB::table('fotos')->orderBy('id','DESC')->get();
+        return view('index', compact('fotos'));
+    }
+
+    public function store(Request $request)
+    {
+        $path = $request->file('imagem')->store('imagens','public');
+
+        $foto = new Foto();
+        $foto->vistoria_id = $request->vistoria_id;
+        $foto->imagem = $path; 
+        $foto->save();
+
+        return redirect('/');
+    }
+
+    public function destroy($id) {
+        $foto = Foto::find($id);
+        if (isset($foto)) {
+            Storage::disk('public')->delete($foto->imagem); 
+            $foto->delete();
+        }
+        return redirect('/');
+    }
+
+    public function download($id) {
+        $foto = Foto::find($id);
+        if (isset($foto)) {
+            $path =  Storage::disk('public')->getDriver()->getAdapter()->applyPathPrefix($foto->imagem);
+            return response()->download($path);
+        }
+        return redirect('/');
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@
 //     return view('index');
 // });
 
-Route::get('/', 'PostControlador@index');
-Route::post('/', 'PostControlador@store');
-Route::delete('/{id}', 'PostControlador@destroy');
-Route::get('/download/{id}', 'PostControlador@download');
+Route::get('/', 'FotoController@index');
+Route::post('/', 'FotoController@store');
+Route::delete('/{id}', 'FotoController@destroy');
+Route::get('/download/{id}', 'FotoController@download');
+
+
 
 
 
